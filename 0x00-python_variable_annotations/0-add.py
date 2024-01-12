429ca2083c30b0d3c47d1ba51d5dba211f750c3e
Karilaw <lawrencekariithi6@gmail.com>
2024-01-12 16:44:46 +0000

    second commit to test

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+This module provides a function
+
+The function `add` takes two arguments
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Add two float numbers.
+
+    Parameters:
+    a (float): The first number.
+    b (float): The second number.
+
+    Returns:
+    float: The sum of the two numbers.
+    """
+    return a + b
