a5da1bfb031496373cea89b1e8cc89dbf0bc1e26
Karilaw <lawrencekariithi6@gmail.com>
2024-02-04 11:01:27 +0000

    first commit to test

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -54,3 +54,35 @@ class TestGetJson(unittest.TestCase):
         mock_get.return_value.json.return_value = test_payload
         self.assertEqual(utils.get_json(test_url), test_payload)
         mock_get.assert_called_once_with(test_url)
+
+
+class TestMemoize(unittest.TestCase):
+    """
+    TestMemoize Class
+    """
+    def test_memoize(self):
+        """
+        test_memoize method
+        """
+        class TestClass:
+            """
+            TestClass
+            """
+            def a_method(self):
+                """
+                a_method method
+                """
+                return 42
+
+            @utils.memoize
+            def a_property(self):
+                """
+                a_property method
+                """
+                return self.a_method()
+
+        with patch.object(TestClass, 'a_method', return_value=42) as mocked:
+            test_class = TestClass()
+            self.assertEqual(test_class.a_property, 42)
+            self.assertEqual(test_class.a_property, 42)
+            mocked.assert_called_once()
