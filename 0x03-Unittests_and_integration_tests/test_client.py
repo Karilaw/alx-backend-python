14ae35b2e8253b030d3fdad7c67a180b198a6f97
Karilaw <lawrencekariithi6@gmail.com>
2024-02-04 11:17:47 +0000

    first commit to test

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -27,3 +27,15 @@ class TestGithubOrgClient(unittest.TestCase):
         mock_get.assert_called_once_with(
             f"https://api.github.com/orgs/{org_name}"
         )
+
+    @patch.object(GithubOrgClient, 'org', new_callable=PropertyMock)
+    def test_public_repos_url(self, mock_org):
+        """
+        test_public_repos_url method
+        """
+        payload = {"repos_url": "World"}
+        mock_org.return_value = payload
+
+        github_org_client = GithubOrgClient("test")
+        expected = payload["repos_url"]
+        self.assertEqual(github_org_client._public_repos_url, expected)
