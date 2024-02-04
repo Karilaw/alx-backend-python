7c3cb08597fdaa09465950474f88e9fef4a31a26
Karilaw <lawrencekariithi6@gmail.com>
2024-02-04 11:47:57 +0000

    first commit to test

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -59,3 +59,15 @@ class TestGithubOrgClient(unittest.TestCase):
             self.assertEqual(repos, ["repo1", "repo2"])
             mock_get.assert_called_once()
             mock_public_repos_url.assert_called_once()
+
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False)
+    ])
+    def test_has_license(self, repo, license_key, expected):
+        """
+        test_has_license method
+        """
+        github_org_client = GithubOrgClient("test")
+        result = github_org_client.has_license(repo, license_key)
+        self.assertEqual(result, expected)
